cbaa1a576f5c15b41a39b9c51be4c72ba7817a81
mycroft440 <alquimistadodetalhe@gmail.com>
2025-07-17 15:15:50 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -52,6 +52,7 @@ install_proxy_single_command( ) {
     # 4. Compilar o projeto Rust
     echo "[INFO] Compilando o projeto Rust...\n"
     # Navega para o diretório onde o Cargo.toml está para compilar
+    source /root/.cargo/env || error_exit "[ERRO] Falha ao carregar o ambiente do Cargo."
     (cd $PROJECT_DIR/src && /root/.cargo/bin/cargo build --release ) &>/dev/null || error_exit "[ERRO] Falha ao compilar o projeto Rust.\n"
     echo "[SUCESSO] Projeto Rust compilado.\n"
 
